9ab7a7742e4f137fbf580377cb2458481f62f94f
kusemererwa <kusemererwaedson2000@gmail.com>
2024-12-07 01:27:57 +0300

    cpanel

--- src/data.jsx
+++ src/data.jsx
@@ -182,7 +182,7 @@ export const testimonial = [
   },
   {
     image:profile5,
-    name:'Precious Stone',
+    name:'Precious `Stone',
     review:` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`
